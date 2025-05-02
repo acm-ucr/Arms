ab3bd153ad1a9cd83e05d9bf8c87213ceb723266
Anthony-Caort <acart040@ucr.edu>
2025-05-02 15:59:23 -0700

    added grip transition logic

--- src/arduino/FSM.ino
+++ src/arduino/FSM.ino
@@ -19,6 +19,7 @@ std::string bufferString;
 // char buffer[5] = {0,0,0,0,0}
 int object_angle;
 int target_angle;
+double arm_angle = 90.0;
 
 void Tick(){
     if(Serial.available() > 0){
@@ -47,7 +48,16 @@ void Tick(){
                       if not reach, continue incrementing 
                       [3, 0/1, 0/1, 0/1, 0/1]
                    */
-            if (buffer[1] == '1' && )
+            int trueCounter = 0;
+            for (unsigned i = 1; i < 5; ++i){
+                if (buffer[i] == '1'){
+                    pressure_reached[i-1] = true;
+                    trueCounter++;
+                }
+            }
+            if (trueCounter == 4){
+                state = Lift;
+            }
             
             break;
         case Lift: /* reach to this state when recieved pressure reached from all 4 pressure sensors
@@ -55,15 +65,13 @@ void Tick(){
             when done, send the flag that we reached up right to MoveToDrop state
             buffer thats being read: [4, 0/1, X, X, X] (X = not used) 
           */
-            if (buffer[1] == '1'){
+            if (arm_angle == 90.0){
                 reached_up_right = true;
+                state = MoveToDrop
             }
             else{
                 reached_up_right = false;
                 state = Lift;
-            }
-            if (reached_up_right){
-                state = MoveToDrop;
             }
         case MoveToDrop:
             break;
